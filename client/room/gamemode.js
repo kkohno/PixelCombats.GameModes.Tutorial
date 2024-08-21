68111d23026771d8e88c6482b5dc40ccc98e7ca7
Konstantin <kkohno@mail.ru>
2024-08-21 21:56:59 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -7,10 +7,10 @@ const TRIGGERS_TAG = "trigger_1";
 const BOTS_SPAWN_TAG = "bots_1";
 //var triggers = room.AreaService.GetByTag(TRIGGERS_TAG);
 var trigger = room.AreaPlayerTriggerService.Get("players_trigger");
-var spawns = room.AreaService.GetByTag(BOTS_SPAWN_TAG);
 trigger.Tags = [TRIGGERS_TAG];
 trigger.Enable = true;
 trigger.OnEnter.Add(function (player, area, trigger) {
+    var spawns = room.AreaService.GetByTag(BOTS_SPAWN_TAG);
     log.Debug(spawns);
     var range = spawns[0].Ranges.All[0];
     var spawn_data = { WeaponId: 2 };
