8ce358fb7e6e4328f6cffe4f3c872dc2f02bc0c4
Konstantin <kkohno@mail.ru>
2024-09-18 22:23:46 +0700

    Update library.js

--- client/room/library.js
+++ client/room/library.js
@@ -14,6 +14,9 @@ trigger_index.Value = 0;
 // задаем размер пула ботов
 room.Bots.PoolSize = BOTS_POOL_SIZE;
 
+// визуализатор триггера игроков
+const players_trigger_view = room.AreaViewService.GetContext().Get("players_trigger_view");
+
 // инициализация всего что зависит от карты
 var trigger_areas = get_areas_by_tag_sorted_by_name(TRIGGERS_TAG);
 var bots_spawns_areas = get_areas_by_tag_sorted_by_name(BOTS_SPAWN_TAG);
@@ -31,8 +34,6 @@ function InitializeFromMap() {
 }
 InitializeFromMap(); // todo регламентировать последовательность отработки режима и карты. Тут чтото нужно придумать, как бы реализовать четкую последовательность отработки скриптов и загрузки карт, возможно стоит сделать какието модули с выгружаемыми дескрипторами соответствующих функций. этот вопрос стоит обсудить с разработчиками режимов и разработчиками игры
 
-// визуализатор триггера игроков
-const players_trigger_view = room.AreaViewService.GetContext().Get("players_trigger_view");
 // триггер игроков
 const players_trigger = room.AreaPlayerTriggerService.Get("players_trigger");
 players_trigger.OnEnter.Add(function (player, area, trigger) {
