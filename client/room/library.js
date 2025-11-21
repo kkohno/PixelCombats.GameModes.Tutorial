4e508c70bbd090c86b9069e4afaebb45a05da773
Konstantin <kkohno@mail.ru>
2025-11-21 16:27:29 +0700

    у ботов только пистолеты

--- client/room/library.js
+++ client/room/library.js
@@ -14,7 +14,8 @@ export const PLAYER_HEAD_HEIGHT = 2.35; // высота середины гол�
 var bots_configured = 0;
 export const trigger_index = room.Properties.GetContext().Get("trigger_index");
 trigger_index.Value = 0;
-const BOT_WEAPONS = [2, 18, 1, 27, 3, 28, 29, 14, 21, 13, 4, 16, 36, 30, 9, 15, 31, 32, 33, 22, 7, 34, 17, 35];
+//const BOT_WEAPONS = [2, 18, 1, 27, 3, 28, 29, 14, 21, 13, 4, 16, 36, 30, 9, 15, 31, 32, 33, 22, 7, 34, 17, 35];
+const BOT_WEAPONS = [1, 27, 3, 36, 17];
 const ROOM_CLOSE_TIME = 10;
 const timer = room.Timers.GetContext().Get("Main");
 
